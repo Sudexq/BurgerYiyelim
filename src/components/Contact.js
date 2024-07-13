eb8b6eab8dc7128189915f5ff8aad3bae5b31358
SudenurTilla <121225010+Sudexq@users.noreply.github.com>
2024-07-13 23:10:46 +0300

    last changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import Banner from "../assets/banner.png";
+import '../styles/Contact.css'
 export const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${Banner})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>iletişim</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Lütfen adınızı soyadınızı giriniz..."
+          />
+          <label>E-mail</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Lütfen emailinizi giriniz..."
+          />
+          <label>Mesajınız</label>
+          <textarea
+            rows="6"
+            name="message"
+            placeholder="Lütfen mesajınızı giriniz..."
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
